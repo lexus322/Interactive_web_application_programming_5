b7d2f22dd88849468a4d0deaea8d2ae2ee51060a
Aleksey Tiurin <shifttygo@gmail.com>
2024-01-24 10:18:24 +0400

    Исправления замечаний на зачете

--- index.js
+++ index.js
@@ -1,56 +1,45 @@
 
 //Создание кнопки закрыть для каждого дела
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u2713");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+let container = document.getElementById("myUL");
+let addButton = document.getElementById("add-button");
+let input = document.getElementById("myInput")
+render();
+
+container.addEventListener("click", (event) => {
+    if (event.target.nodeName === "BUTTON") {
+        let todoID = Number(event.target.getAttribute("data-id"));
+        todos.splice(todoID, 1);
+        render();
+        saveToLocalStorage()
+    }
+});
+
+addButton.addEventListener("click", () => {
+    console.log(input.value)
+    const value = input.value;
+    if (!value) {
+        alert("Невозможно добавить пустое значение");
+        return;
+    }
+    todos.push(value);
+    render();
+    saveToLocalStorage();
+})
+
+function render(){
+    var l = document.getElementById("myUL");
+    let newInnerHtml = todos.reduce((prev, current, index) => {
+        let newValue = `<li>
+            <span>${current}</span>
+            <button data-id=${index}>Удалить<button>
+        </li>`;
+        return prev + newValue;
+
+    }, "");
+   l.innerHTML =newInnerHtml;
 }
 
-//Нажатие на кнопку закрыть для того чтобы скрыть выбранное дело
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-    }
-}
-
-//Добавление символа слева от дела, при выборе выполненного дела
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
-//Добавление нового элемента списка, после нажатия на кнопку добавить
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-        if (inputValue === '') {
-            alert("Вы должны что-то написать!");
-        } else {
-            document.getElementById("myUL").appendChild(li);
-        }
-    document.getElementById("myInput").value = "";
-
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-        }
-    }
+function saveToLocalStorage(){
+    localStorage.setItem("todos",JSON.stringify(todos));
 }
